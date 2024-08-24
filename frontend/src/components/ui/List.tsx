9f12454acf9b457d33cd001991ecf4686f26963d
Tom Ecrepont <tomecrepont@gmail.com>
2024-08-24 16:19:19 +0000

    create the List component

---
+++ frontend/src/components/ui/List.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { Box } from "./Box";
+
+const Root = (props: Omit<Parameters<typeof Box>[0], "as" | "flex" | "flexDirection">) => {
+    return <Box as="ul" flex flexDirection="column" {...props} />
+}
+
+const Item = (props: Omit<Parameters<typeof Box>[0], "as">) => {
+    return <Box as="li" {...props} />
+}
+
+const List = {
+    Root,
+    Item
+}
+
+export { List };

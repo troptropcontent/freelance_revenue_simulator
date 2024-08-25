ddb557da833a644ec841f41469857d8df040b4bf
Tom Ecrepont <tomecrepont@gmail.com>
2024-08-25 13:45:47 +0000

    create the base Token type that we will use as a Theme

---
+++ frontend/src/components/tokens.ts
@@ -0,0 +1,58 @@
+interface Color {
+    light: string;
+    medium: string;
+    dark: string;
+}
+
+interface Tokens {
+    color: {
+        background: {
+            blue: Color;
+            green: Color;
+            red: Color;
+            yellow: Color;
+            orange: Color;
+        };
+        text: {
+            primary: Color;
+            secondary: Color;
+            muted: Color;
+        };
+        border: {
+            blue: Color;
+            green: Color;
+            red: Color;
+            yellow: Color;
+            orange: Color;
+        };
+    },
+    fonts: {
+        family: {
+            default: string;
+            heading: string;
+        };
+        size: {
+            xs: string;
+            sm: string;
+            md: string;
+            lg: string;
+            xl: string;
+        };
+    },
+    spacing: {
+        xs: string;
+        sm: string;
+        md: string;
+        lg: string;
+        xl: string;
+    },
+    borderRadius: {
+        xs: string;
+        sm: string;
+        md: string;
+        lg: string;
+        xl: string;
+    },
+}
+
+export type { Tokens };

6090c9593a1883581227758ba4ff3a53819d8813
Tom Ecrepont <tomecrepont@gmail.com>
2024-08-27 06:53:27 +0000

    setup formik

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,65 +1,116 @@
-import styled from 'styled-components';
-import { Heading } from './components/ui/Heading'
-import { List } from './components/ui/List';
-import { Text } from './components/ui/Text';
-import { Box } from './components/ui/Box';
-import Theme from './components/Theme';
+import styled from "styled-components";
+import { Heading } from "./components/ui/Heading";
+import { List } from "./components/ui/List";
+import { Text } from "./components/ui/Text";
+import { Box } from "./components/ui/Box";
+import Theme from "./components/Theme";
+import { Formik } from "formik";
 
-const MainLayout = styled.div`
-  display: grid;
-  gap: var(--spacing-medium);
+const StyledForm = styled.form`
   padding-inline: var(--spacing-medium);
   padding-block: var(--spacing-large);
-  grid-template-areas:
-    "activities_title results_title"
-    "activities_section results_section";
-
-  #activities_section {
-    grid-area: activities_section;
-  }
-
-  #results_section {
-    grid-area: results_section;
-  }
-
-  #results_title {
-    grid-area: results_title;
-  }
-
-  #activities_title {
-    grid-area: activities_title;
-  }
-
-  #activities_list {
-    max-height: calc(100vh - var(--main-nav-height)*4  );
-    overflow-y: auto;
-  }
+  display: grid;
+  gap: var(--spacing-large);
+  grid-template-columns: 1fr 1fr;
 `;
 
-function App() {
+type FormValues = {
+  freelance_daily_rate?: {
+    rate: number;
+    quantity: number;
+    enjoyment_rate: number;
+  };
+  freelance_on_delivery?: {
+    rate: number;
+    quantity: number;
+    average_time_spent: number;
+    enjoyment_rate: number;
+  };
+  consulting?: {
+    rate: number;
+    quantity: number;
+    enjoyment_rate: number;
+  };
+  sponsorship?: {
+    rate: number;
+    quantity: number;
+    average_time_spent: number;
+    enjoyment_rate: number;
+  };
+  side_project?: {
+    revenue: number;
+    average_time_spent: number;
+    enjoyment_rate: number;
+  };
+  training?: {
+    rate: number;
+    quantity: number;
+    average_time_spent: number;
+    enjoyment_rate: number;
+  };
+  digital_product?: {
+    revenue: number;
+    quantity: number;
+    average_time_spent: number;
+    enjoyment_rate: number;
+  };
+  admin?: {
+    average_time_spent: number;
+  };
+};
 
+const initialValues: FormValues = {
+  freelance_daily_rate: { rate: 0, quantity: 0, enjoyment_rate: 0 },
+  freelance_on_delivery: undefined,
+  consulting: undefined,
+  sponsorship: undefined,
+  side_project: undefined,
+  training: undefined,
+  digital_product: undefined,
+  admin: undefined,
+};
+
+function App() {
   return (
     <Theme>
-    <MainLayout>
-      <Heading as="h2" align="center" id="activities_title">Mes activités</Heading>
-      <List.Root gap="lg" id="activities_list" background='grey.light' borderRadius="md" padding="md">
-        {[1, 2, 3, 4, 5].map((item) => (
-          <List.Item key={item} padding="lg" background='white' borderRadius="sm">
-            <Text>
-              Activité {item}
-            </Text>
-          </List.Item>
-        ))}
-      </List.Root>
-      <Heading as="h2" align="center" id="results_title">Résultats</Heading>
-      <Box background='grey.light' borderRadius="md" flex flexDirection="column" justifyContent="center" alignItems="center">
-        <Text>
-          Résultats
-        </Text>
-      </Box>
-    </MainLayout>
+      <Formik initialValues={initialValues} onSubmit={() => {}}>
+        <StyledForm>
+          <Heading as="h2" align="center" id="activities_title">
+            Mes activités
+          </Heading>
+          <Heading as="h2" align="center" id="results_title">
+            Résultats
+          </Heading>
+          <List.Root
+            gap="lg"
+            id="activities_list"
+            background="grey.light"
+            borderRadius="md"
+            padding="md"
+          >
+            <List.Item
+              key={1}
+              padding="lg"
+              background="white"
+              borderRadius="sm"
+            >
+              <Text>Activité A</Text>
+            </List.Item>
+          </List.Root>
+          <Box
+            background="grey.light"
+            borderRadius="md"
+            flex
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text>Résultats</Text>
+          </Box>
+        </StyledForm>
+      </Formik>
     </Theme>
-  )
+  );
 }
 
-export default App
+export default App;

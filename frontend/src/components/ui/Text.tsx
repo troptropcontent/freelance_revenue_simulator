94f6f88cb05e81d670da4249882871cbf10844c5
Tom Ecrepont <tomecrepont@gmail.com>
2024-08-29 12:55:14 +0000

    improve Text component

--- frontend/src/components/ui/Text.tsx
+++ frontend/src/components/ui/Text.tsx
@@ -1,5 +1,29 @@
-const Text = ({ children }: { children: React.ReactNode }) => {
-  return <p>{children}</p>;
+import { Tokens } from "src/components/tokens";
+import styled from "styled-components";
+import { cssVariable, TextColor } from "src/components/helper";
+
+const StyledText = styled.p<{
+  $size: keyof Tokens["fonts"]["size"];
+  $color: TextColor;
+}>`
+  font-size: ${(props) => cssVariable(`fonts.size.${props.$size}`)};
+  color: ${(props) => cssVariable(`color.text.${props.$color}`)};
+`;
+
+const Text = ({
+  size = "sm",
+  color = "primary.medium",
+  children,
+}: {
+  color?: TextColor;
+  size?: keyof Tokens["fonts"]["size"];
+  children: React.ReactNode;
+}) => {
+  return (
+    <StyledText $size={size} $color={color}>
+      {children}
+    </StyledText>
+  );
 };
 
 export { Text };

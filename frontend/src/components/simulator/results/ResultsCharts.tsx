60ab25e5240ead190757c5c2617c27ce95af8496
Tom Ecrepont <tomecrepont@gmail.com>
2024-09-02 14:23:19 +0000

    fix colors

--- frontend/src/components/simulator/results/ResultsCharts.tsx
+++ frontend/src/components/simulator/results/ResultsCharts.tsx
@@ -13,14 +13,16 @@ import { Box } from "src/components/ui/Box";
 import { Formatter as LegendFormatter } from "recharts/types/component/DefaultLegendContent";
 
 const ChartColors = [
-  "#e76f51", // dark
+  "#5a2900", // darkest
+  "#8b3e00", // darker
+  "#d35400", // dark
   "#ea8a5e", // mediumDark
   "#eda46b", // medium
   "#f0be78", // mediumLight
-  "#f3d885", // light
-  "#f6f292", // lighter
-  "#f9fc9f", // lightest
-  "#fcffac", // pale
+  "#f3d885", // lightDark
+  "#f6f292", // light
+  "#f9fc9f", // lighter
+  "#fcffac", // lightest
 ] as const;
 
 const RADIAN = Math.PI / 180;

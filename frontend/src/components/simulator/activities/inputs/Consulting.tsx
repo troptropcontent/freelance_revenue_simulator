763b81efe6eddfe56a52063084a6f2a24549b73a
Tom Ecrepont <tomecrepont@gmail.com>
2024-10-29 08:50:31 +0000

    remove the previous rule froom sponsorship dan sconsulting as it is not relevant, the hours inputed are not based on week

--- frontend/src/components/simulator/activities/inputs/Consulting.tsx
+++ frontend/src/components/simulator/activities/inputs/Consulting.tsx
@@ -4,16 +4,9 @@ import { EnjoymentRateInput } from "src/components/simulator/activities/private/
 import { useTranslation } from "react-i18next";
 import { CurrencyInputGroup } from "src/components/ui/formik/groups/CurrencyInputGroup";
 import { NumberInputGroup } from "src/components/ui/formik/groups/NumberInput";
-import { useFormikContext } from "formik";
-import { FormValues } from "src/App";
 
 const Consulting = ({ index }: { index: number }) => {
   const { t } = useTranslation();
-  const {
-    values: {
-      config: { number_of_days_worked_per_week },
-    },
-  } = useFormikContext<FormValues>();
 
   return (
     <BaseActivity
@@ -35,7 +28,7 @@ const Consulting = ({ index }: { index: number }) => {
           "simulator.activities.consulting.inputs.average_time_spent.label",
         )}
         min={0}
-        max={number_of_days_worked_per_week}
+        max={20}
         step={0.5}
       />
       <EnjoymentRateInput name={`activities[${index}].values.enjoyment_rate`} />
--- frontend/src/components/simulator/activities/inputs/Sponsorship.tsx
+++ frontend/src/components/simulator/activities/inputs/Sponsorship.tsx
@@ -4,16 +4,10 @@ import { EnjoymentRateInput } from "src/components/simulator/activities/private/
 import { useTranslation } from "react-i18next";
 import { CurrencyInputGroup } from "src/components/ui/formik/groups/CurrencyInputGroup";
 import { NumberInputGroup } from "src/components/ui/formik/groups/NumberInput";
-import { useFormikContext } from "formik";
-import { FormValues } from "src/App";
 
 const Sponsorship = ({ index }: { index: number }) => {
   const { t } = useTranslation();
-  const {
-    values: {
-      config: { number_of_days_worked_per_week },
-    },
-  } = useFormikContext<FormValues>();
+
   return (
     <BaseActivity
       title={t("simulator.activities.sponsorship.label")}
@@ -34,7 +28,7 @@ const Sponsorship = ({ index }: { index: number }) => {
           "simulator.activities.sponsorship.inputs.average_time_spent.label",
         )}
         min={0}
-        max={number_of_days_worked_per_week}
+        max={20}
         step={0.5}
       />
       <EnjoymentRateInput name={`activities[${index}].values.enjoyment_rate`} />

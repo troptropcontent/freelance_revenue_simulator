55524c483ffdecb37c24faad6cdd1e8e38129249
Tom Ecrepont <tomecrepont@gmail.com>
2024-12-12 17:59:58 +0000

    add labels

--- frontend/src/components/simulator/results/charts/WeekChart.tsx
+++ frontend/src/components/simulator/results/charts/WeekChart.tsx
@@ -1,13 +1,13 @@
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { cssVariable } from "src/components/helper";
-import { Box } from "src/components/ui/Box";
-import { BuildPaddingStyle } from "src/components/ui/helpers";
 import styled from "styled-components";
-import { AverageWeekChart } from "./AverageWeekChart";
-import { sample } from "lodash";
-import { Text } from "src/components/ui/Text";
-import { useAverageWeekChartData, useEnjoymentChartData } from "./private/hooks";
+import { useAverageWeekChartData } from "./private/hooks";
 import { useAverageEnjoymentRatePerKindOfActivity } from "../private/hooks";
+import { UNRATED_COLOR } from "src/components/ui/formik/primitives/constants";
+import { Box } from "src/components/ui/Box";
+import { Text } from "src/components/ui/Text";
+import { useFormikContext } from "formik";
+import { FormValues } from "src/App";
 
 const MAXIMUM_NUMBER_OF_DAYS_IN_A_WEEK = 7 as const;
 
@@ -33,15 +33,14 @@ const Container = styled.div`
   --border-size: ${cssVariable("border.sm")};
   --border-radius: ${cssVariable("borderRadius.md")};
   --chart-height: 230px;
-  --left-space-width-for-rates-label: 60px;
+  --chart-weekdays-height: 60px;
+  --left-space-width-for-rates-label: 38px;
   --enjoyment-rate-heart-tile-size: 35px;
 
   display: flex;
   flex-direction: column;
   height: var(--chart-height);
   position: relative;
-  background: white;
-  margin: ${cssVariable("spacing.md")};
   padding-inline-start: var(--left-space-width-for-rates-label);
   border-radius: var(--border-radius);
 `;
@@ -54,7 +53,7 @@ const BackGround = styled.div`
 const Activities = styled.div`
   position: absolute;
   inset: 0;
-  padding-block-start: calc(var(--chart-height) / 7 + var(--border-size));
+  padding-block-start: calc(var(--chart-weekdays-height) + var(--border-size));
   padding-inline-start: var(--left-space-width-for-rates-label);
   display: flex;
   opacity: 0.8;
@@ -73,7 +72,7 @@ const Activity = styled.div<{ $value: number; $color: string }>`
 const EnjoymentRates = styled.div`
   position: absolute;
   inset: 0;
-  padding-block-start: calc(var(--chart-height) / 7 + var(--border-size));
+  padding-block-start: calc(var(--chart-weekdays-height) + var(--border-size));
   padding-inline-start: var(--left-space-width-for-rates-label);
   display: flex;
 `;
@@ -127,7 +126,7 @@ const Weekday = styled.div<{ $weekDay: string }>`
 
   &:before {
     content: "${({ $weekDay }) => $weekDay}";
-    height: calc(var(--chart-height) / 7);
+    height: var(--chart-weekdays-height);
     display: flex;
     align-items: center;
     justify-content: center;
@@ -136,32 +135,58 @@ const Weekday = styled.div<{ $weekDay: string }>`
 const Rates = styled.div`
   position: absolute;
   inset: 0;
-  padding-block-start: calc(var(--chart-height) / 7 + var(--border-size));
+  padding-block-start: calc(var(--chart-weekdays-height) + var(--border-size));
   display: flex;
   flex-direction: column;
 `;
 const Rate = styled.div<{ $rate: number }>`
   flex: 1 1 0px;
-  margin-inline-start: -4px;
-  border-block-start: 2px solid grey;
+  margin-inline-start: -16px;
+  border-block-start: 1px solid grey;
   position: relative;
   &:before {
     content: "${({ $rate }) => $rate} 💛";
     font-size: 13px;
     position: absolute;
     top: -10px;
-    left: -28px;
+    left: -30px;
   }
 `;
 
-const WeekChart = ({ number_of_days = 5}: { number_of_days: number, data: {color: string, value: number, averageEnjoymentRate: number}[] }) => {
-  const weekdays = useLocalisedDaysOfWeek();
+const Label = styled.div<{ $color: string }>`
+  --point-size: 13px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  &:before {
+    display: block;
+    content: " ";
+    height: var(--point-size);
+    width: var(--point-size);
+    border-radius: 50%;
+    background: ${({ $color }) => $color};
+  }
+`;
 
+const WeekChart = () => {
+  const weekdays = useLocalisedDaysOfWeek();
+  const {
+    values: {
+      config: { number_of_days_worked_per_week },
+    },
+  } = useFormikContext<FormValues>();
+  const { t } = useTranslation();
   const { base_data } = useAverageWeekChartData();
-  const averageEnjoymentRateChartData = useAverageEnjoymentRatePerKindOfActivity();
-  const chartData = base_data.map((data) => ({...data, averageEnjoymentRate: averageEnjoymentRateChartData.find(e => e.type == data.type)?.averageEnjoymentRate}))
-  
-  if (number_of_days > MAXIMUM_NUMBER_OF_DAYS_IN_A_WEEK) {
+  const averageEnjoymentRateChartData =
+    useAverageEnjoymentRatePerKindOfActivity();
+  const chartData = base_data.map((data) => ({
+    ...data,
+    averageEnjoymentRate: averageEnjoymentRateChartData.find(
+      (e) => e.type == data.type,
+    )?.averageEnjoymentRate,
+  }));
+
+  if (number_of_days_worked_per_week > MAXIMUM_NUMBER_OF_DAYS_IN_A_WEEK) {
     throw new Error(
       "The number_of_days props passed to WeekChart is above the MAXIMUM_NUMBER_OF_DAYS_IN_A_WEEK",
     );
@@ -176,66 +201,109 @@ const WeekChart = ({ number_of_days = 5}: { number_of_days: number, data: {color
     0,
   );
 
-  const remainingDays = number_of_days - totalNumberOfDays;
-
-  console.log({remainingDays})
-
+  const remainingDays = number_of_days_worked_per_week - totalNumberOfDays;
 
   return (
-    <Container id="WeekChartContainer">
-      <BackGround id="WeekChartBackground">
-        <Weekdays>
-          {[...Array(number_of_days)].map((_, index) => (
-            <Weekday key={index} $weekDay={weekdays[index]} />
-          ))}
-        </Weekdays>
-        <Rates>
-          {[...Array(number_of_days)].map((_, index) => (
-            <Rate $rate={index + 1} />
-          ))}
-        </Rates>
-      </BackGround>
-      <Activities id="WeekChartActivities">
-        {chartData.map((data, i) => (
-          <Activity key={i} $color={data.color} $value={data.value} />
-        ))}
-        {remainingDays > 0 && <Activity key="remaining-activity" $color="grey" $value={remainingDays} />}
-      </Activities>
-      <EnjoymentRates id="WeekChartEnjoymentRates">
-        {[
-          <Block key="first-block" $width={chartData[0].value / 2 / number_of_days} />,
-          ...chartData.slice(0, -1).map((el, i) => (
-            <Block
-              key={`block-${i}`}
-              $width={
-                (el.value / 2 + chartData[i + 1].value / 2) / number_of_days
-              }
-            >
-              <Star $rate={el.averageEnjoymentRate} />
-              <svg height="100%" width="100%">
-                <line
-                  x1="0%"
-                  y1={`${100 - (el.averageEnjoymentRate / 5) * 100}%`}
-                  x2="100%"
-                  y2={`${100 - (chartData[i + 1].averageEnjoymentRate / 5) * 100}%`}
-                  stroke="black"
+    <Box flex flexDirection="column" gap={25}>
+      <Text style="subtitle_n1" align="center">
+        {t("simulator.results.charts.enjoyment_ventilation.title")}
+      </Text>
+      <Box
+        background="white"
+        borderRadius={"md"}
+        padding={30}
+        flex
+        flexDirection="column"
+        gap={25}
+      >
+        <Container id="WeekChartContainer">
+          <BackGround id="WeekChartBackground">
+            <Weekdays>
+              {[...Array(number_of_days_worked_per_week)].map((_, index) => (
+                <Weekday key={index} $weekDay={weekdays[index]} />
+              ))}
+            </Weekdays>
+            <Rates>
+              {[...Array(number_of_days_worked_per_week)].map((_, index) => (
+                <Rate $rate={index + 1} />
+              ))}
+            </Rates>
+          </BackGround>
+          <Activities id="WeekChartActivities">
+            {chartData.map((data, i) => (
+              <Activity key={i} $color={data.color} $value={data.value} />
+            ))}
+            {remainingDays > 0 && (
+              <Activity
+                key="remaining-activity"
+                $color={UNRATED_COLOR}
+                $value={remainingDays}
+              />
+            )}
+          </Activities>
+          <EnjoymentRates id="WeekChartEnjoymentRates">
+            {[
+              <Block
+                key="first-block"
+                $width={chartData[0].value / 2 / number_of_days_worked_per_week}
+              />,
+              ...chartData.slice(0, -1).map((el, i) => (
+                <Block
+                  key={`block-${i}`}
+                  $width={
+                    (el.value / 2 + chartData[i + 1].value / 2) / number_of_days_worked_per_week
+                  }
+                >
+                  <Star $rate={el.averageEnjoymentRate} />
+                  <svg height="100%" width="100%">
+                    <line
+                      x1="0%"
+                      y1={`${100 - (el.averageEnjoymentRate / 5) * 100}%`}
+                      x2="100%"
+                      y2={`${100 - (chartData[i + 1].averageEnjoymentRate / 5) * 100}%`}
+                      stroke="black"
+                    />
+                  </svg>
+                </Block>
+              )),
+              <Block
+                $width={
+                  chartData[chartData.length - 1].value / 2 / number_of_days_worked_per_week
+                }
+              >
+                <Star
+                  $rate={chartData[chartData.length - 1].averageEnjoymentRate}
                 />
-              </svg>
-            </Block>
-          )),
-          <Block
-            $width={
-              chartData[chartData.length - 1].value / 2 / number_of_days
-            }
-          >
-            <Star
-              $rate={chartData[chartData.length - 1].averageEnjoymentRate}
-            />
-          </Block>,
-          ...[remainingDays > 0 ? <Block key="remaining-block" $width={remainingDays / number_of_days}/> : null]
-        ]}
-      </EnjoymentRates>
-    </Container>
+              </Block>,
+              ...[
+                remainingDays > 0 ? (
+                  <Block
+                    key="remaining-block"
+                    $width={remainingDays / number_of_days_worked_per_week}
+                  />
+                ) : null,
+              ],
+            ]}
+          </EnjoymentRates>
+        </Container>
+        <Box flex gap={12} justifyContent="center">
+          {chartData.map((data) => (
+            <Label $color={data.color}>
+              <Text style="text_in_charts">
+                <Trans
+                  i18nKey={`simulator.results.charts.week_ventilation.label`}
+                  values={{
+                    days: t("common.days", { count: data.value }),
+                    kind: t(`simulator.activities.kinds.${data.type}.title`),
+                  }}
+                  components={{ b: <strong /> }}
+                />
+              </Text>
+            </Label>
+          ))}
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
